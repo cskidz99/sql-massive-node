54d28dcfcb84d1c1d9d0f8b296425dc15354c388
Cameron Skidmore <cskidz99@gmail.com>
2016-11-16 14:58:40 -0700

    Copied code from mini to begin

---
+++ index.js
@@ -0,0 +1,34 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var cors = require('cors');
+var massive = require('massive');
+
+var connectionString = "postgres://localhost/sandbox";
+
+var massiveInstance = massive.connectSync({connectionString : connectionString})
+
+// var db = massive.connectSync({
+//   connectionString : 'postgres://localhost/sandbox'
+// });
+
+var app = module.exports = express();
+app.use(bodyParser.json());
+app.use(cors());
+
+app.set('db', massiveInstance);
+var db = app.get('db');
+// app.set('db', db);
+var controller = require('./controller.js');
+
+// db.new_plane(function(err, planes){
+//     console.log(err, "plane added")
+// });
+
+// db.get_planes(function(err, planes){
+//     console.log(err, planes)
+// })
+controller.getPlanes();
+
+app.listen('3000', function(){
+  console.log("Successfully listening on : 3000")
+})
